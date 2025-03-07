eda87cf307b9278dff3d08007a0404c13f537c10
DCCXXV <110636189+DCCXXV@users.noreply.github.com>
2025-03-07 15:33:04 +0100

    Index y redirección a createOrder

---
+++ src/main/java/com/thecritics/reorder/controller/RootController.java
@@ -0,0 +1,33 @@
+package com.thecritics.reorder.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class RootController {
+
+    private static final Logger log = LogManager.getLogger(RootController.class);
+
+    @ModelAttribute
+    public void populateModel(HttpSession session, Model model) {        
+        for (String name : new String[] {"u", "url", "ws"}) {
+            model.addAttribute(name, session.getAttribute(name));
+        }
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/createOrder")
+    public String createOrder(Model model) {
+        return "createOrder";
+    }
+}

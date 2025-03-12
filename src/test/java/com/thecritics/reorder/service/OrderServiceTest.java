6bc07fa670ff6d9b1726947c8813578853973e63
Manuel Villa Abajo <92745912+Manuvi03@users.noreply.github.com>
2025-03-12 11:48:32 +0100

    testDeleteElementFromTheUnassignedTier

--- src/test/java/com/thecritics/reorder/service/OrderServiceTest.java
+++ src/test/java/com/thecritics/reorder/service/OrderServiceTest.java
@@ -24,17 +24,17 @@ class OrderServiceTest {
     private HttpSession session;
 
     /**
-     * Verifica que addElement añade un elemento a la primera lista del estado de la orden.
+     * Verifica que addElement añade un elemento a la tier no ordenada del estado del Order.
      *
-     * <p>Configura un estado inicial con dos listas vacías y verifica que el elemento
+     * <p>Configura un estado inicial con dos tier vacías y verifica que el elemento
      * se añade correctamente a la primera lista.</p>
      */
     @Test
-    void testAddElementShouldAddElementToFirstList() {
+    void testAddElementShouldAddElementToUnassignedTier() {
         // 1. Arrange; creamos las variables
         List<List<String>> initialState = new ArrayList<>();
-        initialState.add(new ArrayList<>());
-        initialState.add(new ArrayList<>());
+        initialState.add(new ArrayList<>()); //Tier 0
+        initialState.add(new ArrayList<>()); //Tier 1
 
         String elementText = "Nuevo elemento";
 
@@ -47,11 +47,21 @@ class OrderServiceTest {
         assertThat(result.getFirst().getFirst()).isEqualTo(elementText);
     }
 
+
+    /**
+     * Verificamos que se elimina un elemento de la assigned tier
+     * 
+     * <p>Configura un estado inicial con dos tier vacías y verifica que el elemento
+     * se elimina correctamente de la assigned tier.</p>
+     * 
+     */
     @Test
-    void deleteElement() {
+    void testDeleteElementFromTheUnassignedTier() {
         // 1. Arrange; Creamos las variables
         List<List<String>> initialState = new ArrayList<>();
-        initialState.add(new ArrayList<>());//Añadimos una Tier
+        initialState.add(new ArrayList<>());//Añadimos la Unassigned Tier
+        initialState.add(new ArrayList<>());//Añadimos la Tier 1
+
         String elementText = "Elemento a eliminar"; //Creamos un elemento
         initialState.getFirst().add(elementText); //Añadimos un elemento a esa Tier
 
@@ -61,6 +71,7 @@ class OrderServiceTest {
 
         // 3. Assert; Comprobamos los resultado
         assertThat(result.getFirst().size()).isEqualTo(0);
+        assertThat(result.get(1).size()).isEqualTo(0);
     }
 
     @Test

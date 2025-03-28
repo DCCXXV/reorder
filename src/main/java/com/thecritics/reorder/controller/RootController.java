b088000e75a0f9d94b184aca8e31231fbaa514e7
Naive <congchanghong@gmail.com>
2025-03-28 05:59:08 +0100

    refactorizar operaciones de createOrder a reorder

--- src/main/java/com/thecritics/reorder/controller/RootController.java
+++ src/main/java/com/thecritics/reorder/controller/RootController.java
@@ -311,5 +311,189 @@ public class RootController {
         model.addAttribute("orderState", orderState);
         return "reorder"; 
 }
+@PostMapping("/reorder/addElement")
+public String reorderaddElement(@RequestParam String elementTextInput, HttpSession session, Model model) {
+    List<List<String>> orderState = orderService.getOrderState(session);
+    String trimmed = elementTextInput.trim();
 
+    if (trimmed.length() > MAX_CHARACTERS) {
+        model.addAttribute("errorMessage", "¡El elemento debe tener menos de 30 caracteres!");
+    } else {
+        if (trimmed.isEmpty()) {
+            model.addAttribute("errorMessage", "¡El elemento no puede estar vacío!");
+        } else {
+            boolean exists = orderState.stream().anyMatch(tier -> tier.contains(trimmed));
+            if (exists) {
+                model.addAttribute("errorMessage", "¡El elemento ya existe!");
+            } else {
+                Integer elementCount = (Integer) session.getAttribute("elementCount");
+
+                if (elementCount == null) {
+                    elementCount = 1;
+                    session.setAttribute("elementCount", elementCount);
+                } else {
+                    elementCount++;
+                    session.setAttribute("elementCount", elementCount);
+                }
+
+                if (elementCount < MAX_ELEMENTS) {
+                    orderState.get(0).add(trimmed);
+                } else {
+                    model.addAttribute("errorMessage", "¡Máximo número de elementos superado (500)!");
+                    elementCount--;
+                    session.setAttribute("elementCount", elementCount);
+                }
+            }
+        }
+    }
+    
+    model.addAttribute("orderState", orderState);
+    return "reorder";
+}
+
+/**
+ * Maneja las solicitudes POST para eliminar un elemento.
+ *
+ * @param elementTextBadge El texto del elemento a eliminar.
+ * @param session          La sesión HTTP actual.
+ * @param model            El objeto Modelo utilizado para pasar datos a la vista.
+ * @return El nombre del fragmento de la vista "createOrder :: #elementsContainer".
+ */
+@PostMapping("/reorder/deleteElement")
+public String reorderdeleteElement(@RequestParam String elementTextBadge, HttpSession session, Model model) {
+    log.debug(elementTextBadge);
+    List<List<String>> orderState = orderService.deleteElement(elementTextBadge, session);
+    model.addAttribute("orderState", orderState);
+
+    Integer elementCount = (Integer) session.getAttribute("elementCount");
+    elementCount--;
+    session.setAttribute("elementCount", elementCount);
+
+    return "reorder";
+}
+
+/**
+ * Maneja las solicitudes POST para añadir un nuevo tier al Order.
+ *
+ * @param model   El objeto Modelo utilizado para pasar datos a la vista.
+ * @param session La sesión HTTP actual.
+ * @param response La respuesta a la petición http.
+ * @returnEl nombre de la vista "createOrder".
+ */
+@PostMapping("/reorder/addTier")
+public String reorderaddTier(Model model, HttpSession session, HttpServletResponse response) {
+    List<List<String>> orderState = orderService.getOrderState(session);
+
+    if (orderState.size() > MAX_TIERS) {
+        model.addAttribute("errorToastMessage", "No se pueden añadir más de 50 Tiers");
+    } else {
+        orderService.addTier(session);
+        model.addAttribute("orderState", orderState);
+        response.setHeader("HX-Trigger", "tierAdded");
+    }
+    
+    return "reorder";
+}
+
+/**
+ * Maneja las solicitudes POST para eliminar el último tier del estado del Order.
+ *
+ * @param session La sesión HTTP actual.
+ * @param model   El objeto Modelo utilizado para pasar datos a la vista.
+ * @return El nombre de la vista "createOrder".
+ */
+@PostMapping("/reorder/keepElementsAndDeleteLastTier")
+public String reorderkeepElementsAndDeleteLastTier(HttpSession session, Model model) {
+    List<List<String>> orderState = orderService.keepElementsAndDeleteLastTier(session);
+    model.addAttribute("orderState", orderState);
+    return "reorder";
+}
+
+/**
+ * Maneja las solicitudes POST para eliminar el último tier del estado del Order.
+ *
+ * @param session La sesión HTTP actual.
+ * @param model   El objeto Modelo utilizado para pasar datos a la vista.
+ * @return El nombre de la vista "createOrder".
+ */
+@PostMapping("/reorder/deleteLastTier")
+public String reorderdeleteLastTier(HttpSession session, Model model) {
+    List<List<String>> orderState = orderService.getOrderState(session);
+    Integer elementCount = (Integer) session.getAttribute("elementCount");
+
+    if (elementCount != null){
+        int n  = orderState.size();
+        List<String> elements = orderState.get(n - 1);
+        elementCount -= elements.size();
+        session.setAttribute("elementCount", elementCount);
+    }   
+    
+    orderState = orderService.deleteLastTier(session);
+    model.addAttribute("orderState", orderState);
+    return "reorder";
+}
+
+/**
+ * Endpoint para actualizar el estado de orden (drag & drop).
+ * Se espera recibir un JSON que representa la nueva lista de tiers.
+ *
+ * @param orderStateJson El JSON que representa la nueva lista de tiers.
+ * @param session        La sesión HTTP actual.
+ * @return El nombre de la vista "createOrder" o "error" en caso de fallo.
+ */
+@PostMapping("/reorder/updateOrderState")
+public String reorderupdateOrderState(@RequestParam String orderStateJson, HttpSession session, Model model) {
+    try {
+        log.debug("Recibido orderStateJson: {}", orderStateJson);
+        List<List<String>> newOrderState = objectMapper.readValue(orderStateJson, new TypeReference<List<List<String>>>() {});
+        log.debug("Enviado newOrderState: {}", newOrderState);
+
+        if (newOrderState.isEmpty()) {
+            return "error";
+        }
+        /*
+        int i = 1;
+        boolean elementsFound = false;
+        while (!elementsFound && i < newOrderState.size()) {
+            if (newOrderState.get(i).size() > 0) {
+                elementsFound = true;
+            }
+            i++;
+        }*/
+
+        //model.addAttribute("publishEnabled", elementsFound);
+        model.addAttribute("orderState", newOrderState);
+        orderService.updateOrderState(newOrderState, session);
+
+        return "reorder";
+
+    } catch (Exception e) {
+        log.error("Error actualizando estado", e);
+        return "error";
+    }
+}
+
+/**
+ * Endpoint para publicar un Order, guardándola en la base de datos.
+ *
+ * @param title   El título del Order.
+ * @param author  El autor del Order.
+ * @param session La sesión HTTP actual, utilizada para obtener el estado del Order.
+ * @return El nombre de la vista "index".
+ */
+@PostMapping("/reorder/PublishOrder")
+public String reorderPublishOrder(@RequestParam String title, @RequestParam String author, HttpSession session, Model model) {
+    if (title == null || title.isEmpty()) {
+        return "error";
+    }
+
+    List<List<String>> orderState = orderService.getOrderState(session);
+
+    orderService.saveOrder(title, author, orderState);
+    orderState = clearOrder(orderState);
+
+    model.addAttribute("toastMessage", "¡Tu Order ha sido publicado correctamente!");
+
+    return "redirect:/";
+}
 }

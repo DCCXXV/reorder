0d82759776e41fb994f4779255b1b893c8cad060
DCCXXV <110636189+DCCXXV@users.noreply.github.com>
2025-04-23 19:58:00 +0200

    fix del merge

--- src/main/java/com/thecritics/reorder/service/OrdererService.java
+++ src/main/java/com/thecritics/reorder/service/OrdererService.java
@@ -5,6 +5,8 @@ import com.thecritics.reorder.model.Orderer;
 import com.thecritics.reorder.repository.OrdererRepository;
 import com.thecritics.reorder.controller.HomeController;
 
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.security.crypto.password.PasswordEncoder;
